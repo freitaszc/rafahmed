2324abd9d8b28a71fae84cb43a0d3aa7c9b06f8b
gabriel <gabrielfreitaszc@gmail.com>
2026-01-14 23:58:48 -0500

    fixed password hashing size

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id              = db.Column(db.Integer, primary_key=True)
     username        = db.Column(db.String(80),  nullable=False, unique=True, index=True)
     email           = db.Column(db.String(120), nullable=False, unique=True, index=True)
-    password_hash   = db.Column(db.String(128), nullable=False)
+    password_hash   = db.Column(db.String(255), nullable=False)
     name            = db.Column(db.String(120))
     birthdate       = db.Column(db.Date)
     profile_image   = db.Column(db.String(200), default='images/user-icon.png')
